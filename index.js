6cbc472027c38e82de894fff2424c088e38c60e5
KİRA CODE <91698866+XDdsng@users.noreply.github.com>
2021-11-19 21:50:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const parzi = require("aoi.js")
+var fs = require('fs')
+const bot = new parzi.Bot({
+    token: "ODI2NTEyMDMyNjk3ODEwOTk1.YGNjUA.MMYR7LN7RbreHbjdfyd5w_Hlzeo",//.env dosyasında token yazan variablenin değerine tokeninizi yazın
+    prefix:"$getServerVar[prefix]"//ayarlamalı prefix 
+})
+bot.onJoined()
+bot.onLeave()
+bot.onMessage()
+var reader = fs.readdirSync("./komutlar/").filter(file => file.endsWith(".js"))
+for(const file of reader) {    
+    const command = require(`./komutlar/${file}`)
+    bot.command({
+        name: command.name,
+        code: command.code
+    })
+}
+
+bot.command({
+  name:"ping",
+  code:`
+  Pingim: **$ping**
+  `
+})
+
+bot.status({
+    text: "Yakında",//buraya durum yazısı
+    type: "PLAYING",//buraya oynuyor bölümü PLAYING LISTENING WATCHING STREAMING şeklindede yapabilirsiniz
+    status: "dnd",//buraya status kısmı dnd idle online şeklinde yapabilirsiniz
+    time: 12
+})
+
+bot.variables({
+  variableismi:"variable değeri",
+  prefix:".",//her zaman "" den sonra , koyun
+  })
+  bot.c
